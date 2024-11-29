becea498e7c9119a1e54325c2fe652e39cfb2aea
JORGE ARAUJO DA SILVA <jorgeara2506@gmail.com>
2024-11-28 22:12:12 -0300

    aula

---
+++ script.js
@@ -0,0 +1,12 @@
+var n1 = parseFloat(prompt("Digite o primeiro numero "))
+var n2 = parseFloat(prompt("Digite o segundo numero"))
+
+if( n1 > n2 ){
+console.log("O maior numero é "+ n1)
+alert("O maior numero é "+ n1)
+}
+
+else{
+    console.log("o maior numero é "+n2)
+    alert("O maior numero é "+n2)
+}
